f7d0758dbc9b7fe751d6463912e8f1a2876c6ea3
DevHanzala <hanzalaghaniabdulghani@gmail.com>
2024-12-13 12:54:15 +0500

    Updated 4 files

--- src/Components/ApplyForm.jsx
+++ src/Components/ApplyForm.jsx
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useToast } from "@/hooks/use-toast"
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 import {
   Form,
   FormControl,
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	"./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}", // For ShadCN
   ],
   theme: {
   	extend: {
